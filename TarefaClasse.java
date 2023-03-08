1b27581766500755cd77fd7df3c9a0a9969267e2
Gustavo Nethson <116599169+GustavoNethson@users.noreply.github.com>
2023-03-08 20:28:19 -0300

    Add files via upload
    Tarefa aula dois

---
+++ TarefaClasse.java
@@ -0,0 +1,45 @@
+/*Criar uma classe distribuindo 5 atributos de um animal e depois criar,
+/* depois criar uma classe para cadastrar 5 animais.*/
+package com.mycompany.tarefaclasse;
+
+public class TarefaClasse {
+
+    
+            public static void main(String[] args){
+                cachorro cachorro1 = new cachorro();
+                cachorro1.nome = "Pluto";
+                cachorro1.corOlhos = "azuis";
+                cachorro1.peso = 53;
+                cachorro1.quantPatas = 4;
+                cachorro1.sexo = "Macho";
+                
+                cachorro cachorro2 = new cachorro();
+                cachorro2.nome = "Rex";
+                cachorro2.corOlhos = "amarelo";
+                cachorro2.peso = 22;
+                cachorro2.quantPatas = 3;
+                cachorro2.sexo = "Femea";
+                
+                cachorro cachorro3 = new cachorro();
+                cachorro3.nome = "bob";
+                cachorro3.corOlhos = "castanho";
+                cachorro3.peso = 13;
+                cachorro3.quantPatas = 4;
+                cachorro3.sexo = "Macho";
+                
+                cachorro cachorro4 = new cachorro();
+                cachorro4.nome = "Suelem Cristina";
+                cachorro4.corOlhos = "verde";
+                cachorro4.peso = 23;
+                cachorro4.quantPatas = 4;
+                cachorro4.sexo = "Macho";
+                
+                cachorro cachorro5 = new cachorro();
+                cachorro5.nome = "Afonso";
+                cachorro5.corOlhos = "castanho";
+                cachorro5.peso = 10;
+                cachorro5.quantPatas = 4;
+                cachorro5.sexo = "Femea";
+            }
+        }
+
---
+++ cachorro.java
@@ -0,0 +1,9 @@
+package com.mycompany.tarefaclasse;
+
+public class cachorro {
+    public String nome;
+    public int peso;
+    public String corOlhos;
+    public int quantPatas;
+    public String sexo;
+}
